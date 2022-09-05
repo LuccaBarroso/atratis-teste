d05cb0780a906cae73260d0b9630350c68065c23
Lucca Barroso <barrosolucca@gmail.com>
2022-09-05 11:06:50 -0300

    fix footer hover

--- wp-content/themes/lucca/footer.php
+++ wp-content/themes/lucca/footer.php
@@ -37,9 +37,9 @@ wp_reset_postdata();
         <div class="col-md-3 d-none d-lg-block">
         </div>
         <div class="col-lg">
-          <div class="d-flex justify-content-lg-end justify-content-center grow-hover align-items-right h-100">
-            <a href="<?php echo $link_facebook?>" class="align-self-center px-2"><svg xmlns="http://www.w3.org/2000/svg"
-                fill="#fff" viewBox="0 0 50 50" width="18px" height="18px">
+          <div class="d-flex justify-content-lg-end justify-content-center align-items-right h-100">
+            <a href="<?php echo $link_facebook?>" class="align-self-center grow-hover px-2"><svg
+                xmlns="http://www.w3.org/2000/svg" fill="#fff" viewBox="0 0 50 50" width="18px" height="18px">
                 <path
                   d="M25,3C12.85,3,3,12.85,3,25c0,11.03,8.125,20.137,18.712,21.728V30.831h-5.443v-5.783h5.443v-3.848 c0-6.371,3.104-9.168,8.399-9.168c2.536,0,3.877,0.188,4.512,0.274v5.048h-3.612c-2.248,0-3.033,2.131-3.033,4.533v3.161h6.588 l-0.894,5.783h-5.694v15.944C38.716,45.318,47,36.137,47,25C47,12.85,37.15,3,25,3z" />
               </svg></a>
@@ -65,7 +65,7 @@ wp_reset_postdata();
           </a>
         </div>
         <div class="col d-flex justify-content-end">
-          <a href="http://www.atratis.com.br">
+          <a href="http://www.atratis.com.br" class="grow-hover">
             <img alt="Atratis Logo" src="<?php echo $logo_atratis?>">
           </a>
         </div>
